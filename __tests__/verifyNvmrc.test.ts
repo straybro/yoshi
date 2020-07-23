53dabf6aa95d09ec7d02fd3664bdee1b05d40aee
Artem Yavorsky <info@yavorsky.org>
2020-07-23 13:31:33 +0300

    Fix test for checking nvmrc files (#2785)

--- __tests__/verifyNvmrc.test.ts
+++ __tests__/verifyNvmrc.test.ts
@@ -6,14 +6,9 @@ import { templates } from '../packages/create-yoshi-app';
 const readNvmrc = (dir: string) =>
   fs.readFileSync(path.join(dir, '.nvmrc')).toString().trim();
 
-const rootNvmrc = readNvmrc(path.resolve(__dirname, '..'));
-
-test("create-yoshi-app's .nvmrc should match yoshi's .nvmrc", () => {
-  const cyaNvmrc = readNvmrc(
-    path.resolve(__dirname, '../packages/create-yoshi-app'),
-  );
-  expect(cyaNvmrc).toBe(rootNvmrc);
-});
+const cyaNvmrc = readNvmrc(
+  path.resolve(__dirname, '../packages/create-yoshi-app'),
+);
 
 templates.forEach(({ name, path: templatePath, availableLanguages }) => {
   availableLanguages.forEach((lang) => {
@@ -21,7 +16,7 @@ templates.forEach(({ name, path: templatePath, availableLanguages }) => {
       lang,
     )} template ${name}'s .nvmrc should match create-yoshi-app's .nvmrc`, () => {
       const templateNvmrc = readNvmrc(path.join(templatePath, lang));
-      expect(templateNvmrc).toBe(rootNvmrc);
+      expect(templateNvmrc).toBe(cyaNvmrc);
     });
   });
 });
