f3da3661067af3d8ed7d5fcb97797c9b8193b133
Artem Yavorsky <info@yavorsky.org>
2020-07-27 20:18:10 +0300

    Add `react-custom-scroll` to be transpiled for editor flow projects. (#2801)
    * Add controller url to dev center
    
    * Fix react-custom-scroll
    
    * add a comment
    
    * remove dev center changes

--- packages/yoshi-flow-editor/src/yoshi.config.js
+++ packages/yoshi-flow-editor/src/yoshi.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  // react-custom-scroll 3.x has untrinspiled entry. wix-base-ui is using it and can't bump the version since it contain some breacking changes.
+  // With this change we are trying to provide an out-of-the-box fix for everyone.
+  externalUnprocessedModules: ['react-custom-scroll'],
   servers: {
     cdn: {
       ssl: true,
