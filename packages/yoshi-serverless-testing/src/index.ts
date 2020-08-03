93e7c615c8f8bd367c081c174ba7a96f4b094052
Ran Yitzhaki <ranyitz@users.noreply.github.com>
2020-08-03 11:50:30 +0300

    get the serverless scope according to the package name (#2862)

--- packages/yoshi-serverless-testing/src/index.ts
+++ packages/yoshi-serverless-testing/src/index.ts
@@ -1,9 +1,10 @@
 const { app } = require('@wix/serverless-testkit');
 const { getServerlessScope } = require('yoshi-helpers/build/utils');
-
-const scope = getServerlessScope('yoshi-serverless-testing');
+const config = require('yoshi-config');
 
 // start the server as an embedded app
 export const bootstrap = () => {
+  const scope = getServerlessScope(config.name);
+
   return app(scope, { appPort: process.env.SERVERLESS_PORT });
 };
