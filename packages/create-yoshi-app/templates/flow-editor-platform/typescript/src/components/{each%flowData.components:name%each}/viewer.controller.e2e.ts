4c7b8e1786639d8ff3b9118c205c87a9152b8248
Artem <bordyanartem@gmail.com>
2020-08-06 20:38:12 +0300

    fix(editor-flow): Fix auto height lib behaviour (#2952)
    * fix(editor-flow): Fix auto height lib behaviour
    
    * fix grammar
    
    * Fix test
    
    * fix(flow-platform): Fix 123
    
    * fix xpath
    
    * fix test.
    
    * fix test.

--- packages/create-yoshi-app/templates/flow-editor-platform/typescript/src/components/{each%flowData.components:name%each}/viewer.controller.e2e.ts
+++ packages/create-yoshi-app/templates/flow-editor-platform/typescript/src/components/{each%flowData.components:name%each}/viewer.controller.e2e.ts
@@ -1,22 +1,25 @@
 import eventually from 'wix-eventually';
 import { viewerUrl } from '../../../dev/sites';
 
+const BUTTON_SELECTOR = '[data-testid=buttonElement]';
+
 describe('Viewer App', () => {
   it('should change text on button click', async () => {
     await page.goto(viewerUrl);
     await page.waitForSelector('h2');
 
     const widgetText = await page.$eval('h2', (node) =>
-      node?.textContent?.toLowerCase(),
+      node?.textContent?.toLowerCase().trim(),
     );
 
     expect(widgetText).toEqual('click the button');
 
-    await page.click('button');
+    await page.waitForSelector(BUTTON_SELECTOR);
+    await page.click(BUTTON_SELECTOR);
 
     eventually(async () => {
       const widgetTextAfterClick = await page.$eval('h2', (node) =>
-        node?.textContent?.toLowerCase(),
+        node?.textContent?.toLowerCase().trim(),
       );
       expect(widgetTextAfterClick).toEqual('you clicked the button');
     });
