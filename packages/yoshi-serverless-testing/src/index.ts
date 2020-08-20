c9eb65b067e011f07527efb2dff47f833d95acdb
Ran Yitzhaki <ranyitz@users.noreply.github.com>
2020-08-20 16:44:15 +0300

    Revert "yoshi-flow-editor: add support for dev/mocks file (#3074)" (#3089)
    This reverts commit 2154ba8531ec6afd34953821b59fba7d2b68b7d9.

--- packages/yoshi-serverless-testing/src/index.ts
+++ packages/yoshi-serverless-testing/src/index.ts
@@ -1,17 +1,15 @@
-import { app } from '@wix/serverless-testkit';
-import { AmbassadorTestkit } from '@wix/ambassador-testkit';
-import { getServerlessScope } from 'yoshi-helpers/build/utils';
-import config from 'yoshi-config';
-import { SERVERLESS_SCOPE_DIR } from 'yoshi-config/build/paths';
+const { app } = require('@wix/serverless-testkit');
+const { getServerlessScope } = require('yoshi-helpers/build/utils');
+const config = require('yoshi-config');
+const paths = require('yoshi-config/build/paths');
 
-export type { AmbassadorTestkit };
 // start the server as an embedded app
 export const bootstrap = () => {
   const scope = getServerlessScope(config.name);
 
   return app({
-    appPort: parseInt(process.env.SERVERLESS_PORT || '', 10),
-    scopePath: SERVERLESS_SCOPE_DIR(scope),
+    appPort: process.env.SERVERLESS_PORT,
+    scopePath: paths.SERVERLESS_SCOPE_DIR(scope),
     scopeName: scope,
   });
 };
